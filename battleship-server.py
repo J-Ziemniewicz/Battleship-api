8f637305af15b41acd86d4bbe428a1889b6e4db3
Fokzterrier <fokzterrier@gmail.com>
2020-09-20 18:55:17 +0200

    Need of change to PEP8 standard

--- battleship-server.py
+++ battleship-server.py
@@ -42,7 +42,7 @@ def createPlayerId():
 class Board:
     def __init__(self):
         self.__hitcount = 17
-        self.__board = np.zeros((10, 10),dtype=np.int)
+        self.__board = np.zeros((10, 10), dtype=np.int)
 
     def setShips(self, posList):
         for pos in posList:
@@ -69,7 +69,7 @@ class GameSession:
         self.__players = []
         self.__boards = {}
         self.addPlayer(playerId)
-        gameList[self.__id]=self
+        gameList[self.__id] = self
 
     def addPlayer(self, playerId):
         if(len(self.__players) < 2):
@@ -78,7 +78,6 @@ class GameSession:
         else:
             print("to many players")
             return 1
-        
 
     def setupBoard(self, playerId, posList):
         self.__boards[playerId] = Board()
@@ -115,8 +114,6 @@ class GameSession:
 
 class Player(tornado.websocket.WebSocketHandler):
 
-    
-
     def open(self):
         print('new connection')
         self.__playerId = createPlayerId()
@@ -145,7 +142,7 @@ class Player(tornado.websocket.WebSocketHandler):
         return self.__playerId
 
     def __getOponentid(self, gameId: int, playerId: int):
-   
+
         for Id in gameList[gameId].getPlayers():
             if Id != playerId:
                 return Id
@@ -155,17 +152,20 @@ class Player(tornado.websocket.WebSocketHandler):
         if msg['type'] == 'playerId':
             print("Removing player %d" % self.__playerId)
             activePlayers.remove(self.__playerId)
-            currentPlayers[int(msg['playerId'])] = currentPlayers.pop(self.__playerId)
+            currentPlayers[int(msg['playerId'])] = currentPlayers.pop(
+                self.__playerId)
             self.__playerId = int(msg['playerId'])
             print("Adding player %d" % self.__playerId)
-            
+
             activePlayers.append(self.__playerId)
             print("Active players %s" % activePlayers)
         elif msg['type'] == 'newGame':
             newGame = GameSession(self.__playerId)
-            self.send_message({'type': 'newGame', 'gameId': newGame.getGameId()})
-        elif msg['type'] == 'exitGame': 
-            oponentId = self.__getOponentid(int(msg['gameId']), self.__playerId)
+            self.send_message(
+                {'type': 'newGame', 'gameId': newGame.getGameId()})
+        elif msg['type'] == 'exitGame':
+            oponentId = self.__getOponentid(
+                int(msg['gameId']), self.__playerId)
             if(oponentId != 0):
                 currentPlayers[oponentId].send_message({'type': 'exitGame'})
             del (gameList[int(msg['gameId'])])
@@ -179,10 +179,11 @@ class Player(tornado.websocket.WebSocketHandler):
             try:
                 if(gameList[msg['gameId']].addPlayer(self.__playerId)):
                     self.send_message({'type': 'joinGame', 'result': 1})
-                    print("Two many players %d" % self.__playerId )
+                    print("Two many players %d" % self.__playerId)
                 else:
                     self.send_message({'type': 'joinGame', 'result': 0})
-                    oponentId = self.__getOponentid(msg['gameId'], self.__playerId)
+                    oponentId = self.__getOponentid(
+                        msg['gameId'], self.__playerId)
                     currentPlayers[oponentId].send_message(
                         {'type': 'joinGame', 'result': 0})
             except KeyError:
@@ -190,21 +191,23 @@ class Player(tornado.websocket.WebSocketHandler):
                 self.send_message({'type': 'joinGame', 'result': 1})
         elif msg['type'] == 'shipSetup':
             if(gameList[int(msg['gameId'])].setupBoard(self.__playerId, msg['shipPos'])):
-                self.send_message({'type': 'gameReady','yourTurn' : True})
-                oponentId = self.__getOponentid(int(msg['gameId']), self.__playerId)
+                self.send_message({'type': 'gameReady', 'yourTurn': True})
+                oponentId = self.__getOponentid(
+                    int(msg['gameId']), self.__playerId)
                 currentPlayers[oponentId].send_message(
-                        {'type': 'gameReady','yourTurn' : False})
-                        
+                    {'type': 'gameReady', 'yourTurn': False})
+
         elif msg['type'] == 'sendTorpedo':
-            oponentId = self.__getOponentid(int(msg['gameId']), self.__playerId)
+            oponentId = self.__getOponentid(
+                int(msg['gameId']), self.__playerId)
             if(oponentId != 0):
                 hitResult = gameList[int(msg['gameId'])].checkHit(
                     oponentId, msg['torpedoPos'])
                 if(hitResult == 2):
                     currentPlayers[self.__playerId].send_message(
-                        {'type': 'gameEnd', 'result': 'won','position': msg['torpedoPos'],'board': 1,'hit':1})
+                        {'type': 'gameEnd', 'result': 'won', 'position': msg['torpedoPos'], 'board': 1, 'hit': 1})
                     currentPlayers[oponentId].send_message(
-                        {'type': 'gameEnd', 'result': 'lost','position': msg['torpedoPos'],'board': 0,'hit':1})
+                        {'type': 'gameEnd', 'result': 'lost', 'position': msg['torpedoPos'], 'board': 0, 'hit': 1})
                     del (gameList[int(msg['gameId'])])
                 else:
                     yourTurn = False
@@ -215,10 +218,11 @@ class Player(tornado.websocket.WebSocketHandler):
                     currentPlayers[oponentId].send_message(
                         {'type': 'torpedo', 'board': 0, 'position': msg['torpedoPos'], 'hit': hitResult, 'yourTurn': not yourTurn})
         elif msg['type'] == 'shipSunk':
-            oponentId = self.__getOponentid(int(msg['gameId']), self.__playerId)
+            oponentId = self.__getOponentid(
+                int(msg['gameId']), self.__playerId)
             if(oponentId != 0):
-                    currentPlayers[oponentId].send_message(
-                        {'type': 'shipSunk','shipPos': msg['shipPos']})
+                currentPlayers[oponentId].send_message(
+                    {'type': 'shipSunk', 'shipPos': msg['shipPos']})
 
 
 class MyApplication(tornado.web.Application):
@@ -243,8 +247,9 @@ if __name__ == "__main__":
     tornado.options.parse_command_line()
     signal.signal(signal.SIGINT, application.signal_handler)
     http_server = tornado.httpserver.HTTPServer(application)
-    port = int(os.getenv('PORT',8888))
-    http_server.listen(port)
+    # port = int(os.getenv('PORT',8888))
+    # http_server.listen(port)
+    http_server.listen(8888)
     myIP = socket.gethostbyname(socket.gethostname())
     print('*** Websocket Server Started at %s***' % myIP)
     tornado.ioloop.PeriodicCallback(application.try_exit, 100).start()
